9ce5f5d38bedd3381c3677a011241ffc24a62165
Cyr Kamga <93388661+OutpaceDataBase@users.noreply.github.com>
2021-10-29 13:42:48 +0000

    Added some printing stuff

--- main.cpp
+++ main.cpp
@@ -5,4 +5,11 @@ using namespace std;
 // P R I N T I N G
 int main() {
   std::cout << "Hello World!\n";
+
+  cout << "My name is ";
+
+  cout << "Ka" << endl;
+
+  cout << "backslash n is most popular with new lines \n right";
+  return 0;
 } 
